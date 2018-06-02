848a2ae426d5057a3e5090711268dd39f961c607
ConnorScoma1 <39754953+ConnorScoma1@users.noreply.github.com>
2018-06-01 22:01:09 -0500

    update

--- page_3/page_3.php
+++ page_3/page_3.php
@@ -39,18 +39,36 @@
 
 
 
-                <main>
+                <div class="form">
                   <p>SEND E-MAIL</p>
-                    <form class="contact-form" action="contactform.php" method="POST">
+                    <form class="contact-form" action="contactform.php" method="post">
                       <input type="text" name="name" placeholder="Full name">
                       <input type="text" name="mail" placeholder="Your e-mail">
                       <input type="text" name="subject" placeholder="Subject">
                         <textarea name="message" placeholder="Message"></textarea>
                         <button type="submit" name="submit">SEND MAIL</button>
 
+                        <?php
+
+                        if (isset($_POST['submit'])){
+                          $name = $_POST['name'];
+                          $subject = $_POST['subject'];
+                          $mailFrom = $_POST['mail'];
+                          $message = $_POST['message'];
+
+                          $mailto = "&#x6d;&#x61;&#105;&#108;&#x74;&#x6f;&#58;&#x63;&#111;&#x6e;&#x6e;&#x6f;&#114;&#115;&#99;&#111;&#109;&#97;&#64;&#103;&#x6d;&#x61;&#105;&#108;&#46;&#99;&#x6f;&#109;";
+                          $headers = "From: ".$mailFrom;
+                          $txt = "You have recived an e-mail from ".$name. ".\n\n".$message;
+
+                          mail($mailto, $subject, $headers, $mailFrom);
+                          header("Location: index.php?mailsend");
+                        }
+
+                        ?>
+
                     </form>
 
-                </main>
+                </div>
 
 
       </div>
