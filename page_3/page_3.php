6b935086bd5bb6b013e495c862da762e2d94006d
ConnorScoma1 <39754953+ConnorScoma1@users.noreply.github.com>
2018-06-01 21:35:46 -0500

    update

---
+++ page_3/page_3.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="../assets/css/page_1.css">
+    <link rel="stylesheet" href="../assets/css/contact_style.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://fonts.googleapis.com/css?family=Bowlby+One+SC|Roboto+Mono" rel="stylesheet">
+    <link rel="shortcut icon" type="image/x-icon" href="../assets/img/icon.png">
+
+      <title>Connor Scoma | Contact Form</title>
+  </head>
+
+    <body>
+      <div class="wrapper">
+
+        <br>
+        <br>
+
+        <section>
+            <nav>
+              <div class="nav">
+                <a class="nav1" href="https://github.com/ConnorScoma1">Github</a>
+                <a class="nav2" href="https://www.linkedin.com/in/connor-scoma-92730331/">Linkedin</a>
+                <a class="nav3" href="../page_1/page.htm">Artwork</a>
+                <a class="nav4" href="../page_2/page_2.htm">Portfolio</a>
+                <a class="nav5" href="../index.htm">Home</a>
+
+              </div>
+            </nav>
+
+        </section>
+          <br>
+
+            <section>
+              <h1 class="Name">Contact Form</h1>
+            </section>
+
+
+
+                <main>
+                  <p>SEND E-MAIL</p>
+                    <form class="contact-form" action="contactform.php" method="POST">
+                      <input type="text" name="name" placeholder="Full name">
+                      <input type="text" name="mail" placeholder="Your e-mail">
+                      <input type="text" name="subject" placeholder="Subject">
+                        <textarea name="message" placeholder="Message"></textarea>
+                        <button type="submit" name="submit">SEND MAIL</button>
+
+                    </form>
+
+                </main>
+
+
+      </div>
+
+<br>
+<br>
+<br>
+
+
+    </body>
+  </html>
