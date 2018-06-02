e294a7abe11d36d37d6b2d24769ea985edb474ac
ConnorScoma1 <39754953+ConnorScoma1@users.noreply.github.com>
2018-06-01 21:29:06 -0500

    Update

--- page_3/contactform.php
+++ page_3/contactform.php
@@ -6,10 +6,12 @@ if (isset($_POST['submit'])){
   $mailFrom = $_POST['mail'];
   $message = $_POST['message'];
 
-  $mailto = "connorscoma@live.com";
+  $mailto = "&#x6d;&#x61;&#105;&#108;&#x74;&#x6f;&#58;&#x63;&#111;&#x6e;&#x6e;&#x6f;&#114;&#115;&#99;&#111;&#109;&#97;&#64;&#103;&#x6d;&#x61;&#105;&#108;&#46;&#99;&#x6f;&#109;";
   $headers = "From: ".$mailFrom;
   $txt = "You have recived an e-mail from ".$name. ".\n\n".$message;
 
   mail($mailto, $subject, $headers, $mailFrom);
   header("Location: index.php?mailsend");
 }
+
+?>
